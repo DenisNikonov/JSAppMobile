af204b3aca29b30f20512d8d814ae9b1f750bae0
DenisNikonov <53314176+DenisNikonov@users.noreply.github.com>
2019-12-23 17:57:44 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Setup/>
+</template>
+
+<script>
+
+import Setup from './src/boot/setup.vue';
+
+export default {
+  components: {
+    Setup
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Root>
+    <view>
+      <text>Fdfsdafasdfd</text>
+    </view>
+  </Root>
+</template>
+
+
+<script>
+import { Root } from "native-base"
+export default {
+  components: { Root }
+}
+</script>
